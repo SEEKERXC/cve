b2cffc2586c74df721241e1701b32b659f623a54
xiao fei <xiaofei3@xiaomi.com>
2021-11-15 15:46:30 +0800

    'bugfix'

--- auto_make_mask.py
+++ auto_make_mask.py
@@ -14,8 +14,6 @@ import clang.cindex
 from lxml import etree
 from hashlib import blake2b
 
-sSecurityBulletinUri = r'https://nvd.nist.gov/vuln/detail/'
-
 project_path = 'C:/Users/13319/PycharmProjects/autoMakeMask/'
 
 FILE_EXISTS_TEMPLATE = {
@@ -68,6 +66,12 @@ VERSION_LEVEL = {
 # 解析cve补丁页面以获取被修改的函数名
 # returns: 受补丁影响的函数名、代码文件名、代码完整路径、cve特征、修改后的代码的所有函数名、发布日期
 def process_cve(cveid):
+    bulletin_url = ''
+    with open("cves-complete.json", 'r') as f:
+        result = json.loads(f.read())['result']
+    for item in result:
+        if item['CVE'] == cveid:
+            bulletin_url = item['url']
     cvefeature = CVE_TEMPLATE.copy()
     cvefeature['cve'] = cveid
 
@@ -75,43 +79,11 @@ def process_cve(cveid):
         'http': 'socks5://127.0.0.1:10010',
         'https': 'socks5://127.0.0.1:10010'
     }
-    uri = '{}{}'.format(sSecurityBulletinUri, cveid)
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
                  'Chrome/51.0.2704.103 Safari/537.36 '
     headers = {"User-Agent": user_agent}
-    html_r = requests.get(uri, timeout=60, headers=headers, proxies=proxies)
-    html_text = etree.HTML(html_r.text)
-    wpsc = str(etree.tostring(html_text, pretty_print=True))
     diffuris = []
-    gp = re.findall(r'(http://source.android.com/security/bulletin.*?)[\"<]', wpsc)
-    if not gp:
-        gp = re.findall(r'(https://source.android.com/security/bulletin.*?)[\"<]', wpsc)
-    if not gp:
-        diffuris = re.findall(r'(https://android.googlesource.com/platform/.*?)\"', wpsc)
-    if gp == [] and diffuris == []:
-        diffuris = re.findall(r'(http://android.googlesource.com/platform/.*?)\"', wpsc)
-    if gp == [] and diffuris == []:
-        print('[*][func=searchCVEInfo]Error: Can\'t found {} Diff web page address in {}.'.format(cveid, uri))
-        search_url = 'https://cse.google.com/cse/element/v1'
-        params = {
-            'rsz': 'filtered_cse',
-            'num': 10,
-            'hl': 'en',
-            'source': 'gcsc',
-            'gss': '.com',
-            'cselibv': 'cc267ab8871224bd',
-            'cx': '016258643462168859875:qqpm8fiwgc0',
-            'q': cveid,
-            'safe': 'off',
-            'cse_tok': 'AJvRUv0cZaTCYrG86F2lGagUcZLg:1636093256173',
-            'exp': 'csqr,cc,4618907',
-            'callback': 'google.search.cse.api19986'
-        }
-        search_request = requests.get(search_url, params=params,
-                                      timeout=60, headers=headers, proxies=proxies)
-        json_search = search_request.text
-        index_p = json_search.index('(')
-        gp.append(json.loads(json_search[(index_p + 1):-2])['results'][0]['url'])
+
     func_names = []  # 被修改的函数名
     func_list = []  # 所有函数名
     filenames = []  # 代码文件名
@@ -122,151 +94,148 @@ def process_cve(cveid):
     upverid = None
     levels = None
     patch_date = None
-    if gp:
-        bulletin_url = gp[0]
-        if str(bulletin_url).endswith(','): bulletin_url = bulletin_url[:-1]
-        html_r2 = requests.get(bulletin_url, timeout=60, headers=headers, proxies=proxies)
-        html_text2 = etree.HTML(html_r2.text)
-        tbs = html_text2.xpath('//table')
 
-        tags = re.findall(r'([a-zA-Z \n\-\.\(\)]{0,})</p>\n{0,3}\<table\>(.*?)\</table\>', html_r2.text, re.S)
-        summary: str = None
-        for t in tags:
-            if cveid in t[1]:
-                summary = t[0].replace('\n', ' ')
-                break
-        if summary and '<p>' in summary:
-            summary = summary.split('<p>')[-1]
-        cvefeature['title'] = summary
-        # 解析公告页面，提出diff链接
-        hasuri = False
-        for tb in tbs:
-            trs = tb.xpath('.//tr')
-            for tr in trs:
-                ths = tr.xpath('.//th')
-                for thid in range(len(ths)):
-                    if ths[thid].text == "Severity": serveid = thid
-                    if ths[thid].text == "Updated AOSP versions":
-                        upverid = thid
-                    elif ths[thid].text == "Updated versions":
-                        upverid = thid
-                tds = tr.xpath('.//td')
-                for tdid in range(len(tds)):
-                    if len(tds) < 3: continue
-                    if tds[tdid].text != cveid: continue
-                    diffuris = [a.get('href') for a in tds[tdid + 1].xpath('.//a')]
-                    print(diffuris)
+    html_r2 = requests.get(bulletin_url, timeout=60, headers=headers, proxies=proxies)
+    html_text2 = etree.HTML(html_r2.text)
+    tbs = html_text2.xpath('//table')
 
-                    isgoogleuri = False
-                    for link in diffuris:
-                        if 'android.googlesource.com' not in link: continue
-                        isgoogleuri = True
-                        link = str(bulletin_url)
-                        patch_date = link[(link.rindex('/') + 1):]
-                        cvefeature['category'] = patch_date[0:7]
-                    if not isgoogleuri: continue
-                    hasuri = True
-                    levels = tds[serveid].text
-                    updated_versions = tds[upverid].text.split(', ')
-                    print("影响版本：{}".format(updated_versions))
-        if not hasuri: return ''
-        oneid = 0
-        for vr in updated_versions:
-            if vr.startswith("4"): oneid += 1
-        cvefeature['minApiLevel'] = int(VERSION_LEVEL[str(float(updated_versions[oneid][:3]))])
-        cvefeature['maxApiLevel'] = int(VERSION_LEVEL[str(float(updated_versions[-1][:3]))])
-        cvefeature['severity'] = levels
+    tags = re.findall(r'([a-zA-Z \n\-\.\(\)]{0,})</p>\n{0,3}\<table\>(.*?)\</table\>', html_r2.text, re.S)
+    summary: str = None
+    for t in tags:
+        if cveid in t[1]:
+            summary = t[0].replace('\n', ' ')
+            break
+    if summary and '<p>' in summary:
+        summary = summary.split('<p>')[-1]
+    cvefeature['title'] = summary
+    # 解析公告页面，提出diff链接
+    hasuri = False
+    for tb in tbs:
+        trs = tb.xpath('.//tr')
+        for tr in trs:
+            ths = tr.xpath('.//th')
+            for thid in range(len(ths)):
+                if ths[thid].text == "Severity": serveid = thid
+                if ths[thid].text == "Updated AOSP versions":
+                    upverid = thid
+                elif ths[thid].text == "Updated versions":
+                    upverid = thid
+            tds = tr.xpath('.//td')
+            for tdid in range(len(tds)):
+                if len(tds) < 3: continue
+                if tds[tdid].text != cveid: continue
+                diffuris = [a.get('href') for a in tds[tdid + 1].xpath('.//a')]
+                print(diffuris)
 
-        # diff页面
-        for uri in diffuris:
-            if 'https' not in uri or 'http' not in uri:
-                uri = 'https:' + uri
-            html_r3 = requests.get(uri, timeout=60, headers=headers, proxies=proxies)
-            html_text3 = etree.HTML(html_r3.text)
-            aa = html_text3.xpath('//a')
-            diff_detail = ''
-            for a in aa:
-                if a.text != 'diff': continue
-                diff_detail = 'https://android.googlesource.com{}'.format(a.get('href'))
-                break
-            print('==================diff详情链接==================')
-            print(diff_detail)
+                isgoogleuri = False
+                for link in diffuris:
+                    if 'android.googlesource.com' not in link: continue
+                    isgoogleuri = True
+                    link = str(bulletin_url)
+                    patch_date = link[(link.rindex('/') + 1):]
+                    cvefeature['category'] = patch_date[0:7]
+                if not isgoogleuri: continue
+                hasuri = True
+                levels = tds[serveid].text
+                updated_versions = tds[upverid].text.split(', ')
+                print("影响版本：{}".format(updated_versions))
+    if not hasuri: return None, None, None, None, None, None
+    oneid = 0
+    for vr in updated_versions:
+        if vr.startswith("4"): oneid += 1
+    cvefeature['minApiLevel'] = int(VERSION_LEVEL[str(float(updated_versions[oneid][:3]))])
+    cvefeature['maxApiLevel'] = int(VERSION_LEVEL[str(float(updated_versions[-1][:3]))])
+    cvefeature['severity'] = levels
 
-            # 找到代码链接以及对应的行数
-            html_r4 = requests.get(diff_detail, timeout=60, headers=headers, proxies=proxies)
-            html_text4 = etree.HTML(html_r4.text)
-            aa = html_text4.xpath('//a')
-            aa_texts = []
-            for a in aa:
-                aa_texts.append(a.text)
+    # diff页面
+    for uri in diffuris:
+        if 'https' not in uri or 'http' not in uri:
+            uri = 'https:' + uri
+        html_r3 = requests.get(uri, timeout=60, headers=headers, proxies=proxies)
+        html_text3 = etree.HTML(html_r3.text)
+        aa = html_text3.xpath('//a')
+        diff_detail = ''
+        for a in aa:
+            if a.text != 'diff': continue
+            diff_detail = 'https://android.googlesource.com{}'.format(a.get('href'))
+            break
+        print('==================diff详情链接==================')
+        print(diff_detail)
 
-            print('==================代码文件名与链接==================')
-            for a in aa:
-                text = a.text
-                if isinstance(text, str) and text.startswith("b/") \
-                        and (text.endswith('.cc') or text.endswith('.c')
-                             or text.endswith('.cpp')):
-                    if ('a/' + text[2:]) in aa_texts:  # 仅对修改过的c/c++代码
-                        post_url = 'https://android.googlesource.com{}'.format(a.get('href'))
-                        patched_code_links.append(post_url)
-                        name = text[(text.rindex('/') + 1):]
-                        filenames.append(name)
-                        print('file name:  ', name)
-                        print('post-patch: ', post_url)
-                        for link in aa:
-                            if link.text == 'a/' + text[2:]:
-                                pre_url = 'https://android.googlesource.com{}'.format(link.get('href'))
-                                original_code_links.append(pre_url)
-                                print('pre-patch:  ', pre_url)
-            spans = html_text4.xpath("//span[@class='Diff-hunk']")
-            linenos = []  # 找到修改后的代码对应的行数
-            for span in spans:
-                index_start = str(span.text).index('+') + 1
-                index_end = str(span.text).index(',', index_start)
-                t = str(span.text)[index_start:index_end]
-                lineno = int(t)
-                linenos.append(lineno)
+        # 找到代码链接以及对应的行数
+        html_r4 = requests.get(diff_detail, timeout=60, headers=headers, proxies=proxies)
+        html_text4 = etree.HTML(html_r4.text)
+        aa = html_text4.xpath('//a')
+        aa_texts = []
+        for a in aa:
+            aa_texts.append(a.text)
 
-            for i in range(len(original_code_links)):
-                original_link = original_code_links[i]
-                patched_link = patched_code_links[i]
-                name = filenames[i]
+        print('==================代码文件名与链接==================')
+        for a in aa:
+            text = a.text
+            if isinstance(text, str) and text.startswith("b/") \
+                    and (text.endswith('.cc') or text.endswith('.c')
+                         or text.endswith('.cpp')):
+                if ('a/' + text[2:]) in aa_texts:  # 仅对修改过的c/c++代码
+                    post_url = 'https://android.googlesource.com{}'.format(a.get('href'))
+                    patched_code_links.append(post_url)
+                    name = text[(text.rindex('/') + 1):]
+                    filenames.append(name)
+                    print('file name:  ', name)
+                    print('post-patch: ', post_url)
+                    for link in aa:
+                        if link.text == 'a/' + text[2:]:
+                            pre_url = 'https://android.googlesource.com{}'.format(link.get('href'))
+                            original_code_links.append(pre_url)
+                            print('pre-patch:  ', pre_url)
+        spans = html_text4.xpath("//span[@class='Diff-hunk']")
+        linenos = []  # 找到修改后的代码对应的行数
+        for span in spans:
+            index_start = str(span.text).index('+') + 1
+            index_end = str(span.text).index(',', index_start)
+            t = str(span.text)[index_start:index_end]
+            lineno = int(t)
+            linenos.append(lineno)
 
-                html_r_pre = requests.get(original_link, timeout=60, headers=headers, proxies=proxies)
-                html_r_post = requests.get(patched_link, timeout=60, headers=headers, proxies=proxies)
-                html_text_pre = etree.HTML(html_r_pre.text)
-                html_text_post = etree.HTML(html_r_post.text)
+        for i in range(len(original_code_links)):
+            original_link = original_code_links[i]
+            patched_link = patched_code_links[i]
+            name = filenames[i]
 
-                # 从代码页面提取出代码，保存到文件中
-                trs_pre = html_text_pre.xpath('//tr')
-                trs_post = html_text_post.xpath('//tr')
+            html_r_pre = requests.get(original_link, timeout=60, headers=headers, proxies=proxies)
+            html_r_post = requests.get(patched_link, timeout=60, headers=headers, proxies=proxies)
+            html_text_pre = etree.HTML(html_r_pre.text)
+            html_text_post = etree.HTML(html_r_post.text)
 
-                f_pre = open('pre_' + name, 'w')
-                for tr in trs_pre:
-                    code_spans = tr.xpath('td[2]/span')
-                    line = ''
-                    for s in code_spans:
-                        line += s.text
-                    f_pre.write("{}\n".format(line))
-                f_pre.flush()
-                f_pre.close()
+            # 从代码页面提取出代码，保存到文件中
+            trs_pre = html_text_pre.xpath('//tr')
+            trs_post = html_text_post.xpath('//tr')
 
-                f_post = open('post_' + name, 'w')
-                for tr in trs_post:
-                    code_spans = tr.xpath('td[2]/span')
-                    line = ''
-                    for s in code_spans:
-                        line += s.text
-                    f_post.write("{}\n".format(line))
-                f_post.flush()
-                f_post.close()
+            f_pre = open('pre_' + name, 'w', encoding="utf-8")
+            for tr in trs_pre:
+                code_spans = tr.xpath('td[2]/span')
+                line = ''
+                for s in code_spans:
+                    line += s.text
+                f_pre.write("{}\n".format(line))
+            f_pre.flush()
+            f_pre.close()
 
-                # 从代码文件找到修改行对应的函数
-                for lineno in linenos:
-                    func_name, func_list = find_func_name_by_lineno('post_' + name, lineno)
-                    if func_name and func_name not in func_names:
-                        func_names.append(func_name)
+            f_post = open('post_' + name, 'w', encoding="utf-8")
+            for tr in trs_post:
+                code_spans = tr.xpath('td[2]/span')
+                line = ''
+                for s in code_spans:
+                    line += s.text
+                f_post.write("{}\n".format(line))
+            f_post.flush()
+            f_post.close()
 
+            # 从代码文件找到修改行对应的函数
+            for lineno in linenos:
+                func_name, func_list = find_func_name_by_lineno('post_' + name, lineno)
+                if func_name and func_name not in func_names:
+                    func_names.append(func_name)
     return func_names, filenames, patched_code_links, cvefeature, func_list, patch_date
 
 
@@ -462,7 +431,7 @@ if __name__ == '__main__':
         file_exists_digests = []
         mask_digests_list = {}
         affected_funcs, code_file_names, code_links, cvefeature, func_list, patch_date = process_cve(cve)
-        if len(affected_funcs) == 0:
+        if not affected_funcs or len(affected_funcs) == 0:
             print('=============={}：没找到受影响函数=============='.format(cve))
             exit(0)
         print('Affected function names: ', affected_funcs)
